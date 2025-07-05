532fca6f1bd317707568175bace7d9766883e69d
pramod-wdo <pramodhc007@gmail.com>
2025-07-06 01:33:25 +0530

    adds view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_07_05_141043) do
-  create_schema "extensions"
+  # create_schema "extensions"
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
