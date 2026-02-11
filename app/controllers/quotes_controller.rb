2c3efd2664509e1845984b78c7bad6a3deff15d1
pramod-wdo <pramodhc007@gmail.com>
2026-02-11 11:32:37 +0530

    adds progress bar and other changes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < ApplicationController
 	before_action :set_quote, only: [:show, :edit, :update, :destroy]
-
+	before_action -> { sleep 3}
 	def index
 		@quotes = Quote.all
 	end
